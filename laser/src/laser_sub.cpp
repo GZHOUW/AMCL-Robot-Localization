e44772b60d3d9258e7116539d2dac3b9b20a0771
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2022-02-06 16:12:36 -0500

    Add files via upload

---
+++ laser/src/laser_sub.cpp
@@ -0,0 +1,47 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+class Laser{
+private:
+    ros::NodeHandle node_handle;
+    ros::Subscriber laser_sub;
+
+public:
+    Laser(ros::NodeHandle& nh){
+        node_handle = nh;
+        // call callback function in constructor
+        laser_sub = nh.subscribe("laser_topic", 1000, &Laser::laser_callback, this);
+    }
+
+    void laser_callback(const sensor_msgs::LaserScan::ConstPtr& s){
+        // parse the received laser scan data
+        float a_min = s->angle_min; // start angle of the scan
+
+        float increment = s-> angle_increment;
+        std::vector<float> ranges = s->ranges;
+    
+        float detect_r_max, detect_a_min, detect_a_max;
+        float sum = 0;
+        int size = 0;
+
+        for (int i = 0; i < ranges.size(); i++){
+            float r = ranges[i];
+            float a = a_min + i * increment;
+
+            // discard ranges that are out of bound
+            if (r < s->range_min or r > s->range_max){
+                continue;
+            }
+
+            if (r < detect_r_max and a > detect_a_min and a < detect_a_max){
+                sum+=r;
+                size++;
+            }
+        }
+
+        float dist = sum/size;
+        
+        ROS_INFO_STREAM(dist);
+        
+    }
+};
